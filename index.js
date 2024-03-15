3c01531a8acc16b60e27ecb26bdcbeaa3eacc384
Hanna Skairipa <83796834+PinkQween@users.noreply.github.com>
2024-03-15 13:04:28 -0600

    please help with polls

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const rwClient = require("./client");
 
-const tweet = async (text) => {
+const tweet = async (text, options, duration) => {
     try {
-        await rwClient.v2.tweet(text);
+        const poll = {
+            duration_minutes: duration,
+            options
+        }
+
+        await rwClient.v2.tweet(text, poll);
     } catch (err) {
         console.log(err);
     }
 }
 
-// tweet("Test");
+const tweetPoll = async (text) => {
+    try {
+        await rwClient.v2.tweet
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+tweetPoll("Test poll" ["1", "2", "3", "4", "5"], 1);
